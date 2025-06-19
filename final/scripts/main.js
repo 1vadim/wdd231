569e696e5fe095a28dcca973952d56a0677fd2f5
1vadim <vchemariev@byupathway.edu>
2025-06-19 11:33:50 +0200

    week06-error9

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,22 +1,19 @@
-import { loadEvents } from './events.js';
-import { setupEventModal } from './modal.js';
+import { loadEvents } from "./events.js";
+import { setupEventModal } from "./modal.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.hamburger');
-  const navMenu = document.getElementById('nav-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.getElementById("nav-menu");
 
   if (hamburger && navMenu) {
-    hamburger.addEventListener('click', () => {
-      const expanded = hamburger.getAttribute('aria-expanded') === 'true';
-      hamburger.setAttribute('aria-expanded', !expanded);
-      navMenu.classList.toggle('open');
+    hamburger.addEventListener("click", () => {
+      const expanded = hamburger.getAttribute("aria-expanded") === "true";
+      hamburger.setAttribute("aria-expanded", !expanded);
+      navMenu.classList.toggle("open");
     });
   }
 
+  loadEvents("event-list");
 
-  loadEvents('event-list');
-  
-
-  
-  setupEventModal('event-modal', 'event-list');
+  setupEventModal("event-modal", "event-list");
 });
